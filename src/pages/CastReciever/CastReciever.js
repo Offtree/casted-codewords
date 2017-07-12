ee4ade4991b0609d41a40ddbfd9659b9370441b2
David Rowe <david.t.rowe@slalom.com>
2017-07-11 22:35:20 -0500

    moved around some stuff

--- src/pages/CastReciever/CastReciever.js
+++ src/pages/CastReciever/CastReciever.js
@@ -6,12 +6,9 @@ import CastedBackdrop from '../../atoms/CastedBackdrop';
 class CastReciever extends Component {
   constructor(props) {
     super(props);
-    this.castMananger = window.cast.receiver.CastReceiverManager.getInstance();
-    this.messageBus = this.castMananger.getCastMessageBus(
-      'urn:x-cast:com.google.cast.codenames.action'
-    );
-    this.castMananger.start();
+    this.CastReceiverManager = window.cast.receiver.CastReceiverManager.getInstance();
     this.setUpEvents();
+    this.CastReceiverManager.start();
   
     this.state = {
       message: 'None Yet'
@@ -19,13 +16,17 @@ class CastReciever extends Component {
   }
 
   setUpEvents() {
+    this.messageBus = this.castMananger.getCastMessageBus(
+      'urn:x-cast:com.google.cast.codenames.action'
+    );
+
     // handler for 'senderdisconnected' event
-    this.castMananger.onSenderDisconnected = function(event) {
-      if (this.castMananger.getSenders().length === 0) window.close();
+    this.CastReceiverManager.onSenderDisconnected = function(event) {
+      if (this.CastReceiverManager.getSenders().length === 0) window.close();
     };
     
     // handler for messageBus
-    this.messageBus.onMessage = function(event) {
+    this.messageBus.onMessage = (event) => {
       this.messageBus.send(event.senderId, event.data);
       this.dispatch(event.data);
       this.setState({
